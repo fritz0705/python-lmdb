0b59f3c582c9ac263d667349e86d7a6c0c155bb6
Fritz Grimpen <fritz@grimpen.net>
2013-09-05 14:39:39 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="python-lmdb",
+	version="1.0dev",
+	packages=[
+		"lmdb"
+	],
+	author="Fritz Grimpen",
+	author_email="fritz@grimpen.net",
+	url="http://github.com/fritz0705/python-lmdb.git",
+	license="http://opensource.org/licenses/MIT",
+	description="simple lmdb bindings written using ctypes",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Operating System :: POSIX",
+		"Programming Language :: Python :: 3.3",
+		"Topic :: Database :: Database Engines/Servers"
+	],
+	long_description=long_description,
+	intsall_requires=[]
+)
