b094eeafb3514d7e354b40ab6f6cbf686670da3a
Fritz Grimpen <fritz@grimpen.net>
2013-09-05 14:21:30 +0000

    Add docstrings

--- lmdb.py
+++ lmdb.py
@@ -6,9 +6,11 @@ import ctypes
 import ctypes.util
 
 class Error(Exception):
+	"""Extended Exception class for LMDB exceptions which decodes bytes objects
+	by default."""
 	def __init__(self, msg):
 		if isinstance(msg, bytes):
-			msg = msg.encode()
+			msg = msg.decode()
 		Exception.__init__(self, msg)
 
 class Stat(ctypes.Structure):
@@ -31,6 +33,9 @@ class Value(ctypes.Structure):
 	_fields_ = [("mv_size", ctypes.c_size_t),
 		("mv_data", ctypes.c_void_p)]
 
+	def to_bytes(self):
+		return ctypes.string_at(self.mv_data, self.mv_size)
+	
 	@classmethod
 	def from_bytes(cls, b):
 		self = cls()
@@ -49,6 +54,9 @@ class Value(ctypes.Structure):
 		return cls.from_bytes(obj)
 
 class LibLMDB(object):
+	"""Instances of this class represents open shared library handles to a
+	liblmdb.so and enables access to it's low-level methods."""
+
 	def __init__(self, lib=None):
 		if lib is None:
 			lib = ctypes.util.find_library("lmdb")
@@ -64,6 +72,9 @@ class LibLMDB(object):
 
 	@staticmethod
 	def _monkey_patch_lib(lib):
+		"""Monkey patch supplied ctypes library object to adjust return and argument
+		types."""
+
 		# mdb_version
 		lib.mdb_version.restype = ctypes.c_char_p
 		lib.mdb_version.argtypes = [ctypes.POINTER(ctypes.c_int),
@@ -201,6 +212,8 @@ class LibLMDB(object):
 			ctypes.POINTER(Value), ctypes.POINTER(Value)]
 
 	def version(self):
+		"""Obtain version of MDB binding and return 4-tuple of major, minor, patch
+		level and version string."""
 		major, minor, patch = ctypes.c_int(), ctypes.c_int(), ctypes.c_int()
 		res = self._lib.mdb_version(ctypes.pointer(major),
 			ctypes.pointer(minor),
@@ -208,9 +221,11 @@ class LibLMDB(object):
 		return major.value, minor.value, patch.value, res
 
 	def strerror(self, errno):
+		"""Return string error message for provided error number."""
 		return self._lib.mdb_strerror(errno).decode()
 
 	def env_create(self):
+		"""Create new environment handle and return."""
 		val = ctypes.c_void_p()
 		err = self._lib.mdb_env_create(ctypes.pointer(val))
 		if err != 0:
@@ -218,6 +233,7 @@ class LibLMDB(object):
 		return val
 
 	def env_open(self, env, path, flags, mode):
+		"""Associate environment handle with path."""
 		if isinstance(path, str):
 			path = path.encode()
 		err = self._lib.mdb_env_open(env, path, flags, mode)
@@ -225,16 +241,19 @@ class LibLMDB(object):
 			raise Error(self.strerror(err))
 	
 	def env_copy(self, env, path):
+		"""Copy environment to provided path."""
 		err = self._lib.mdb_env_copy(env, path)
 		if err != 0:
 			raise Error(self.strerror(err))
 
 	def env_copyfd(self, env, fd):
+		"""Copy environment to provided file descriptor."""
 		err = self._lib.mdb_env_copyfd(env, fd)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def env_stat(self, env):
+		"""Return Stat object from environment handle."""
 		res = Stat()
 		err = self._lib.mdb_env_stat(env, ctypes.pointer(res))
 		if err != 0:
@@ -242,6 +261,7 @@ class LibLMDB(object):
 		return res
 	
 	def env_info(self, env):
+		"""Return EnvInfo object from environment handle."""
 		res = EnvInfo()
 		err = self._lib.mdb_env_info(env, ctypes.pointer(res))
 		if err != 0:
@@ -249,19 +269,23 @@ class LibLMDB(object):
 		return res
 	
 	def env_sync(self, env, force):
+		"""Sync environment."""
 		err = self._lib.mdb_env_sync(env, force)
 		if err != 0:
 			raise Error(self.strerror(err))
 
 	def env_close(self, env):
+		"""Destroy environment handle."""
 		self._lib.mdb_env_close(env)
 
 	def env_set_flags(self, env, flags, onoff):
+		"""Set flags for environment handle."""
 		err = self._lib.mdb_env_set_flags(env, flags, onoff)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def env_get_flags(self, env):
+		"""Get flags for environment handle."""
 		res = ctypes.c_uint()
 		err = self._lib.mdb_env_get_flags(env, ctypes.pointer(res))
 		if err != 0:
@@ -269,6 +293,7 @@ class LibLMDB(object):
 		return res.value
 
 	def env_get_path(self, env):
+		"""Return associated path from environment handle."""
 		res = ctypes.c_char_p()
 		err = self._lib.mdb_env_get_path(env, ctypes.pointer(res))
 		if err != 0:
@@ -276,16 +301,19 @@ class LibLMDB(object):
 		return res.value.decode()
 
 	def env_set_mapsize(self, env, size):
+		"""Set mapping size for environment handle."""
 		err = self._lib.mdb_env_set_mapsize(env, size)
 		if err != 0:
 			raise Error(self.strerror(err))
 
 	def env_set_maxreaders(self, env, readers):
+		"""Set maximum readers for environment handle."""
 		err = self._lib.mdb_env_set_maxreaders(env, readers)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def env_get_maxreaders(self, env):
+		"""Get maximum readers for environment handle."""
 		res = ctypes.c_uint()
 		err = self._lib.mdb_env_get_maxreaders(env, ctypes.pointer(res))
 		if err != 0:
@@ -293,15 +321,19 @@ class LibLMDB(object):
 		return res.value
 
 	def env_set_maxdbs(self, env, dbs):
+		"""Set maximum database count for environment handle."""
 		err = self._lib.mdb_env_set_maxdbs(env, dbs)
 		if err != 0:
 			raise Error(self.strerror(err))
 
 	def env_get_maxkeysize(self, env):
+		"""Get maximum key size for environment handle."""
 		res = self._lib.mdb_env_get_maxkeysize(env)
 		return res
 
 	def txn_begin(self, env, parent, flags):
+		"""Create transaction handle from environment, optional parent transaction,
+		and flags."""
 		res = ctypes.c_void_p()
 		err = self._lib.mdb_txn_begin(env, parent, flags, ctypes.pointer(res))
 		if err != 0:
@@ -309,26 +341,32 @@ class LibLMDB(object):
 		return res
 
 	def txn_env(self, txn):
+		"""Return environment handle for transaction handle."""
 		res = self._lib.mdb_txn_env(txn)
 		return res
 
 	def txn_commit(self, txn):
+		"""Commit and invalidate transaction handle."""
 		err = self._lib.mdb_txn_commit(txn)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def txn_abort(self, txn):
+		"""Abort and invalidate transaction handle."""
 		self._lib.mdb_txn_abort(txn)
 	
 	def txn_reset(self, txn):
+		"""Reset transaction handle."""
 		self._lib.mdb_txn_reset(txn)
 	
 	def txn_renew(self, txn):
+		"""Prepare transaction handle for reuse after reset."""
 		err = self._lib.mdb_txn_renew(txn)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def dbi_open(self, txn, name, flags):
+		"""Open database handle by transaction handle, optional name and flags."""
 		if isinstance(name, str):
 			name = name.encode()
 		res = ctypes.c_uint()
@@ -338,6 +376,7 @@ class LibLMDB(object):
 		return res
 
 	def stat(self, txn, dbi):
+		"""Return Stat object for database handle."""
 		res = Stat()
 		err = self._lib.mdb_stat(txn, dbi, ctypes.pointer(res))
 		if err != 0:
@@ -345,6 +384,7 @@ class LibLMDB(object):
 		return res
 
 	def dbi_flags(self, txn, dbi):
+		"""Return flags for database handle."""
 		res = ctypes.c_uint()
 		err = self._lib.mdb_dbi_flags(txn, dbi, ctypes.pointer(res))
 		if err != 0:
@@ -352,14 +392,18 @@ class LibLMDB(object):
 		return res
 
 	def dbi_close(self, txn, dbi):
+		"""Close database handle."""
 		self._lib.mdb_dbi_close(txn, dbi)
 
 	def drop(self, txn, dbi, delete=False):
+		"""Empty database if delete is False or delete database from enviroment and
+		close transaction handle if delete is True."""
 		err = self._lib.mdb_drop(txn, dbi, delete)
 		if err != 0:
 			raise Error(self.strerror(err))
 
 	def get(self, txn, dbi, key):
+		"""Get items from database handle."""
 		res = Value()
 		err = self._lib.mdb_get(txn, dbi, ctypes.pointer(key), ctypes.pointer(res))
 		if err != 0:
@@ -367,27 +411,39 @@ class LibLMDB(object):
 		return res
 
 	def put(self, txn, dbi, key, value, flags):
+		"""Put item into database."""
 		err = self._lib.mdb_put(txn, dbi, ctypes.pointer(key), ctypes.pointer(value), flags)
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 	def delete(self, txn, dbi, key, value):
+		"""Delete item from database."""
 		err = self._lib.mdb_del(txn, dbi, ctypes.pointer(key), ctypes.pointer(value))
 		if err != 0:
 			raise Error(self.strerror(err))
 	
 class Environment(object):
+	"""Instances of this class represents an environment handle and provide higher
+	level access to it's properties."""
+
 	def __init__(self, lib):
 		self._lib = lib
-		self._handle = lib.env_create()
+		self.create()
 
 	def __del__(self):
 		self.close()
 
-	def open(self, path, flags, mode):
+	def create(self):
+		"""Create environment handle for this environment. This is done on __init__,
+		but is necessary after closing."""
+		self._handle = lib.env_create()
+
+	def open(self, path, flags=0, mode=0o644):
+		"""Associate this environment handle with a database path."""
 		self._lib.env_open(self._handle, path, flags, mode)
 
 	def close(self):
+		"""Close environment handle. You have to recreate an environment handle."""
 		if self._handle is None:
 			self._lib.env_close(self._handle)
 			self._handle = None
@@ -398,40 +454,54 @@ class Environment(object):
 	def copyfd(self, fd):
 		self._lib.env_copyfd(self._handle, fd)
 	
+	@property
 	def stat(self):
+		"""Return Stat object for this environment."""
 		return self._lib.env_stat(self._handle)
 
+	@property
 	def info(self):
+		"""Return EnvInfo object for this environment."""
 		return self._lib.env_stat(self._handle)
 
 	def sync(self, force=False):
+		"""Sync this environment."""
 		self._lib.env_sync(self._handle, force)
 
 	def set_flags(self, flags, on_off=False):
+		"""Set flags for this environment."""
 		self._lib.env_set_flags(self._handle, flags, on_off)
 	
 	def get_flags(self):
+		"""Get flags from this environment."""
 		return self._lib.env_get_flags(self._handle)
 
 	def get_path(self):
+		"""Return associated path for this environment."""
 		return self._lib.env_get_path(self._handle)
 
 	def set_mapsize(self, size):
+		"""Set mapping size of this environment."""
 		self._lib.env_set_mapsize(self._handle, size)
 	
 	def set_maxreaders(self, maxreaders):
+		"""Set maximum readers count for this environment."""
 		self._lib.env_set_maxreaders(self._handle, maxreaders)
 	
 	def get_maxreaders(self):
+		"""Get maximum readers count for this environment."""
 		return self._lib.env_get_maxreaders(self._handle)
 
 	def set_maxdbs(self, maxdbs):
+		"""Set maximum database count for this environment."""
 		self._lib.env_set_maxdbs(self._handle, maxdbs)
 
 	def get_maxkeysize(self):
+		"""Get maximum key size for this environment."""
 		return self._lib.env_get_maxkeysize(self._handle)
 
 	def transaction(self, flags=0):
+		"""Create new transaction for this environment."""
 		return Transaction(self, flags=flags)
 
 	@property
@@ -459,47 +529,78 @@ class Environment(object):
 		return self.get_maxkeysize()
 
 class Transaction(object):
+	"""Instances of Transaction represents an open transaction."""
+
+	_database = None
+
 	def __init__(self, env, parent=None, flags=0, lib=None):
 		if lib is None:
 			lib = env._lib
 		self._lib = lib
 		self.env = env
-		self._handle = self._lib.txn_begin(env._handle,
-			parent._handle if parent is not None else None,
-			flags)
-	
+		self.begin(parent, flags)
+
 	def __del__(self):
 		self.abort()
 
 	def __enter__(self):
+		if self._handle is None:
+			raise Error("Transaction is finished")
 		return self
 
 	def __exit__(self, exc_type, exc_value, traceback):
 		if exc_type is None:
 			self.commit()
+			self.begin()
 		else:
 			self.abort()
+			self.begin()
+	
+	def begin(self, parent=None, flags=0):
+		"""Begin new transaction by allocating a transaction handle."""
+		self._handle = self._lib.txn_begin(env._handle,
+			parent._handle if parent is not None else None,
+			flags)
 
 	def transaction(self, flags=0):
+		"""Return new sub-transaction from this transaction."""
 		return Transaction(self.env, self, flags)
 
 	def commit(self):
+		"""Commit this transaction. After committing it you have to rebegin it."""
 		if self._handle is not None:
 			self._lib.txn_commit(self._handle)
 			self._handle = None
 	
 	def abort(self):
+		"""Abort this transaction. After aborting it you have to rebegin it."""
 		if self._handle is not None:
 			self._lib.txn_abort(self._handle)
 			self._handle = None
 
 	def reset(self):
+		"""Reset this transaction."""
 		self._lib.txn_reset(self._handle)
 
 	def renew(self):
+		"""Renew this transaction after resetting it."""
 		self._lib.txn_renew(self._handle)
 
+	def database(self, name=None, flags=0):
+		"""Return database object for the associated environment."""
+		return Database(self, name, flags)
+
+	@property
+	def primary_database(self):
+		"""Get primary database for the associated environment."""
+		if self._primary_database is None:
+			self._primary_database = self.database()
+		return self._primary_database
+
 class Database(object):
+	"""Instances of Database represents database handles which are
+	associated with a transaction and an environment."""
+
 	def __init__(self, transaction, name, flags=0, lib=None):
 		if lib is None:
 			lib = transaction._lib
@@ -517,27 +618,34 @@ class Database(object):
 		self.close()
 
 	def stat(self):
+		"""Return Stat for database handle."""
 		return self._lib.stat(self.transaction._handle, self._handle)
 
 	def flags(self):
+		"""Return flags for database handle."""
 		return self._lib.dbi_flags(self.transaction._handle, self._handle)
 
 	def close(self):
+		"""Close this database handle."""
 		self._lib.dbi_close(self.transaction._handle, self._handle)
 
 	def empty(self):
+		"""Empty this database."""
 		self._lib.drop(self.transaction._handle, self._handle, False)
 
 	def drop(self):
+		"""Drop this database and close it."""
 		self._lib.drop(self.transaction._handle, self._handle, True)
 
 	def get(self, key):
+		"""Get item from database."""
 		if not isinstance(key, Value):
 			key = Value.from_object(key)
 		res = self._lib.get(self.transaction._handle, self._handle, key)
-		return ctypes.string_at(res.mv_data, res.mv_size)
+		return res.to_bytes()
 
 	def put(self, key, value, flags=0):
+		"""Put item into database."""
 		if not isinstance(key, Value):
 			key = Value.from_object(key)
 		if not isinstance(value, Value):
@@ -545,6 +653,7 @@ class Database(object):
 		self._lib.put(self.transaction._handle, self._handle, key, value, flags)
 
 	def delete(self, key, value=None):
+		"""Delete item from database."""
 		if not isinstance(key, Value):
 			key = Value.from_object(key)
 		if value is not None and not isinstance(value, Value):
@@ -563,7 +672,4 @@ class Database(object):
 	@property
 	def env(self):
 		return self.transaction.env
-
-if __name__ == "__main__":
-	main()
 
